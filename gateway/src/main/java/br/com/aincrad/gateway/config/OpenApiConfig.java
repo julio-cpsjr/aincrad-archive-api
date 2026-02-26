0cdfcf99160b8229ca59b19a65fb50aa7156f3e6
julio-cpsjr <juliocpsjunior@gmail.com>
2026-02-26 17:48:25 -0300

    Feat(gateway): Implements gateway CRUD endpoints

---
+++ gateway/src/main/java/br/com/aincrad/gateway/config/OpenApiConfig.java
@@ -0,0 +1,19 @@
+package br.com.aincrad.gateway.config;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class OpenApiConfig {
+
+    @Bean
+    public OpenAPI gatewayOpenAPI() {
+        return new OpenAPI()
+                .info(new Info()
+                        .title("Gateway API")
+                        .description("Documentação central dos microserviços")
+                        .version("v1.0"));
+    }
+}
